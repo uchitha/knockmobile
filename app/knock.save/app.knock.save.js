6e226fb733c9618692295f34018acacf24ca6733
unknown <ranac9@AG578068.apac.ent.bhpbilliton.net>
2014-11-30 16:26:59 +0800

    Knock save success failure messaging

--- app/knock.save/app.knock.save.js
+++ app/knock.save/app.knock.save.js
@@ -1,19 +1,37 @@
 (function () {
 
 	angular.module('app')
-			.controller('SaveKnock', SaveKnock);
+			.controller('SaveKnock', SaveKnock)
+			.controller('Modal', Modal);
 
 
-	SaveKnock.$inject = ['$location','knockservice'];
+	SaveKnock.$inject = ['$location','$modal','knockservice'];
+	Modal.$inject = ['$location', '$modalInstance', 'result'];
 
-	function SaveKnock($location, knockservice) {
+	function Modal($location, $modalInstance, result) {
+		var vm = this;
+		vm.knockResult = result;
+		vm.goToMainPage = goToMainPage;
+
+		function goToMainPage() {
+			$modalInstance.close('close');
+			if (vm.knockResult.success) {
+				$location.path('/');
+			}
+		}
+	}
+
+	function SaveKnock($location, $modal, knockservice) {
 		var vm = this;
 		vm.rakeNumber = knockservice.getRake();
 		vm.carList = [];
 		vm.selectAllToggle = false; //UnSelect All to b checked
+		vm.showMsgWindow = false;
+		vm.knockResult = null;
 
 		vm.activate = activate;
 		vm.selectAll = selectAll;
+		vm.saveKnock = saveKnock;
 
 		activate();
 
@@ -43,6 +61,44 @@
 				});
 			}
 		}
+
+		function saveKnock() {
+			//knockService.saveKnock(vm.rakeNumber, vm.carList.filter(function (car) { return car.selected;}));
+
+			//knockSucces();
+			knockFailed();
+
+			function knockSucces() {
+				vm.knockResult = {
+					success : true,
+					title: "Success",
+					message : "Knock successfully saved"
+				}
+				showWindow();
+			}
+
+			function knockFailed() {
+				vm.knockResult = {
+					success : false,
+					title: "Failure",
+					message : "Knock save failed. Please try again."
+				}
+				showWindow();
+			}
+		}
+
+		function showWindow() {
+			vm.modalInstance = $modal.open( {
+				templateUrl: "knockSaveMessage.html",
+				controller: "Modal as vm",
+				resolve: {
+					result: function () {
+						return vm.knockResult;
+					}
+				}
+			});
+		}
+		
 	}
 
 })();
